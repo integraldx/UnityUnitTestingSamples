a05dddf9028f582738a3636b49665be47fe2b5b6
integraldx <integraldx@integraldx.me>
2020-09-01 05:28:44 +0900

    Introducing new tests

---
+++ Assets/Scripts/Player.Tests/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HelloWorld
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void HelloWorldSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
